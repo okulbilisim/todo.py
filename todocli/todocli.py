12cc9d89ecc9799151ce9ddf1c2e33e512b90549
hasantayyar <tayyar.besik@gmail.com>
2014-07-06 23:43:09 +0300

    todo.py with sqlite3 bootstrap

--- todocli/todocli.py
+++ todocli/todocli.py
@@ -4,9 +4,19 @@ __version__ = "0.0.1@alpha"
 
 
 import sys
+import sqlite3
 
 
 def main():
-    print("Executing todocli version %s." % __version__)
-    print("List of argument strings: %s" % sys.argv[1:])
+  print("Executing todocli version %s." % __version__)
+  print("List of argument strings: %s" % sys.argv[1:])
 
+
+def create_todo_db():
+  conn = sqlite3.connect('todo.db')
+  print "Created todo.db on home dir.";
+  conn.execute('''CREATE TABLE TODOPY
+  (ID INT PRIMARY KEY     NOT NULL,
+  TITLE           TEXT    NOT NULL,
+  DEADLINE        INT     NOT NULL,
+  PRIORITY        INT     NOT NULL);''')
