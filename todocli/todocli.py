55451ea1b074fb92d091658a52f09340c86d2104
hasantayyar <tayyar.besik@gmail.com>
2014-07-06 23:24:44 +0300

    application bootstrap with setup tools

---
+++ todocli/todocli.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+__version__ = "0.0.1@alpha"
+
+
+import sys
+
+
+def main():
+    print("Executing todocli version %s." % __version__)
+    print("List of argument strings: %s" % sys.argv[1:])
+
